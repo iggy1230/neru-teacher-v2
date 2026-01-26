fb4070541e364a306457b4a7fdd3e6353daa3769
iggy1230 <jojo.iggy@gmail.com>
2026-01-26 19:58:17 +0900

    Add files via upload

---
+++ public/js/config/firebase-config.js
@@ -0,0 +1,10 @@
+// --- firebase-config.js (このファイルはGitHubにアップロードしない！) ---
+const firebaseConfig = {
+  apiKey: "AIzaSyCQ-RMDyxHy7j9PcIRoSPAkOCET6Vws-uk",
+  authDomain: "nerusensei-19b48.firebaseapp.com",
+  projectId: "nerusensei-19b48",
+  storageBucket: "nerusensei-19b48.firebasestorage.app",
+  messagingSenderId: "236770762272",
+  appId: "1:236770762272:web:6ad03298d7d0faae7e6645",
+  measurementId: "G-WV5REMN31P"
+};
